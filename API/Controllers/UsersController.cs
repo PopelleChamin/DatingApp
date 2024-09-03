09cf3253d9703ab9fd4da72e8cbf313038a9a8ba
Erick René Herrera González <mynama97@gmail.com>
2024-09-02 21:48:01 -0600

    Parcial01_AddAsyncCode

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -18,18 +19,24 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers(){
-        var users= _context.Users.ToList();
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersAsync(){
+        var users= await _context.Users.ToListAsync();
 
-        return Ok(users);
+        return users;
     }
-    [HttpGet("{id}")] // api/v1/users/2
-    public ActionResult<AppUser> GetUsersById(int id){
+    [HttpGet("{id:int}")] // api/v1/users/2
+    public async Task<ActionResult<AppUser>> GetUsersByIdAsync(int id){
 
-        var user= _context.Users.Find(id);
+        var user= await _context.Users.FindAsync(id);
 
         if (user == null) return NotFound();
 
         return user;
+    }
+
+    [HttpGet("{name}")] // api/v1/users/Calamardo
+    public ActionResult<string> Ready(string name){
+
+        return $"Hi {name}";
     }
 }
