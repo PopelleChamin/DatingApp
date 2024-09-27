a10fbda1ac3989623f66f24d72781408ef28a195
Erick René Herrera González <mynama97@gmail.com>
2024-09-27 16:05:03 -0600

    Parcial01_AddHomePage

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
-import { Observable } from 'rxjs';
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -23,7 +23,7 @@ ngOnInit(): void {
   this.setCurrentUser();
 }
 
-setCurrentUser(){
+setCurrentUser(): void{
   const userString =  localStorage.getItem("user");
   if(!userString) return;
   const user = JSON.parse(userString);
