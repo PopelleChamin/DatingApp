39351bd1e40e3f085036d0239d0318a2da48bead
Erick René Herrera González <162051457+PopelleChamin@users.noreply.github.com>
2024-10-04 20:11:26 -0600

    Parcial01-CodeToFirstEvaluation (#1)
    Squash and merge:
    
    * first commit
    
    * .gitignore Fixed
    
    * Prueba01_InicialConfiguration
    
    * Parcial01_FirstEntity
    
    * Parcial01_AddEntityFrameworkCore
    
    * Parcial01_AddDataContext
    
    * Parcial01_AddFirtsefMigration
    
    * Parcial01_AddDatabaseUsingDotnetEFDatabaseComands
    
    * Parcial01_UserController
    
    * Parcial01_AddUserController
    
    * Parcial01_AddAsyncCode
    
    * Parcial01_InitialAngularCommit
    
    * Parcial01_AddAngularComponent
    
    * Parcial01_AddCORS
    
    * Parcial01_AddUserListToHtml
    
    * Parcial01_Add_ngx-bootstrap@12 bootstrap font-awesome
    
    * Parcial01_Create-mkcert-AndSSL
    
    * Parcial01_AddPasswordToAppuser
    
    * Parcial01_AddApiControllerBase
    
    * Parcial01_AddRegisterAsync
    
    * Parcial01_AddDTOs
    
    * Parcial01_AddLoginEndpoint
    
    * Parcial01_AddTokenService
    
    * Parcial01_AddAuthnMiddleware
    
    * Parcial01_AddAuthnMiddleware
    
    * Parcial01_AddExtensionMethods
    
    * Parcial01_AddExtensionMethods
    
    * Parcial01_AddNavBarToAngularSPA
    
    * Parcial01_AngularTemplateForms
    
    * Parcial01_InjectAccountServiceIntoComponent
    
    * Parcial01_AddConditionlsToShow/RemoveContentFromDOM
    
    * Parcial01_AddBootstrapDropdown
    
    * Parcial01_PersistTheLogin
    
    * Parcial01_AddHomePage
    
    * Parcial01_AddRegisterComponent
    
    * Parcial01_PARENTtoCHILDComunication
    
    * Parcial01_CHILDtoPARENTComunication
    
    * Parcial01_AddUserRegistration

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+namespace API.Entities;
+
+public class AppUser{
+    public int Id{ get; set; }
+
+    public required string UserNane { get; set; }
+    public required byte[] PasswordHash {get; set;}
+    public required byte[] PasswordSalt {get; set;}
+}
+
