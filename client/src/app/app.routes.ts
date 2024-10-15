50ccb1e6aa59c51b217d7ec947718bbc5377df07
Erick René Herrera González <mynama97@gmail.com>
2024-10-15 16:16:55 -0600

    Parcial02 - Add not-found an server-error landing pages

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { authGuard } from './_guards/auth.guard';
 import { TestsErrorsComponent } from './errors/tests-errors/tests-errors.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 export const routes: Routes = [
     {path:"",component: HomeComponent},
@@ -21,5 +23,7 @@ export const routes: Routes = [
         ]
     },
     {path: "errors", component: TestsErrorsComponent},
+    {path: "not-found", component: NotFoundComponent},
+    {path: "server-error", component: ServerErrorComponent},
     {path:"*",component: MessagesComponent, pathMatch: "full"},
 ];
