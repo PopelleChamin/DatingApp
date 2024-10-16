1fa50b565b1f693b991058e7393e32665acb1f41
Erick René Herrera González <mynama97@gmail.com>
2024-10-16 15:34:31 -0600

    Parcial02 - Add EF Relationships

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -2,7 +2,8 @@ namespace API.Data;
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
-public class DataContext(DbContextOptions options ) : DbContext( options ){
-    public DbSet<AppUser> Users {get; set;}
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
 }
 
