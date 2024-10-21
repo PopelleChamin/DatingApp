22b4bae2597900cae922d41db832daa6dbda66ef
Erick René Herrera González <mynama97@gmail.com>
2024-10-21 15:25:47 -0600

    Parcial02 - Add AutoMapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 namespace API.Controllers;
 using API.Data;
 using API.DataEntities;
+using API.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,7 +24,7 @@ public class UsersController : BaseApiController
     }
 
     [HttpGet("{id:int}")] // api/users/2
-    public async Task<ActionResult<AppUser>> GetUsersByIdAsync(int id)
+    public async Task<ActionResult<MemberResponse>> GetUsersByIdAsync(int id)
     {
 
         var user = await _repository.GetByIdAsync(id);
@@ -37,7 +38,7 @@ public class UsersController : BaseApiController
     }
 
     [HttpGet("{username}")] // api/users/Calamardo
-    public async Task<ActionResult<AppUser>> GetByUsernameAsync(string username)
+    public async Task<ActionResult<MemberResponse>> GetByUsernameAsync(string username)
     {
 
         var user = await _repository.GetByUsernameAsync(username);
