f076472650326ea9a6e2647835a20bee8d9001d4
Erick René Herrera González <mynama97@gmail.com>
2024-10-21 15:11:56 -0600

    Parcial02 - Update users controller and add MemberResponse

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -2,14 +2,25 @@ namespace API.Data;
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using API.Entities;
+using API.DataEntities;
 using Microsoft.EntityFrameworkCore;
 
 public class UserRepository(DataContext context) : IUserRepository
 {
-    public async Task<IEnumerable<AppUser>> GetAllAsync() => await context.Users.ToListAsync();
-    public async Task<AppUser?> GetByIdAsync(int id) => await context.Users.FindAsync(id);
-    public async Task<AppUser?> GetByUsernameAsync(string username) => await context.Users.SingleOrDefaultAsync(x => x.UserNane == username);
-    public async Task<bool> SaveAllAsync() => await context.SaveChangesAsync() > 0;
-    public void Update(AppUser user) => context.Entry(user).State = EntityState.Modified;
+    public async Task<IEnumerable<AppUser>> GetAllAsync()
+        => await context.Users
+                .Include(u => u.Photos)
+                .ToListAsync();
+    public async Task<AppUser?> GetByIdAsync(int id)
+        => await context.Users
+                .Include(u => u.Photos)
+                .FirstOrDefaultAsync(u => u.Id == id);
+    public async Task<AppUser?> GetByUsernameAsync(string username)
+        => await context.Users
+                .Include(u => u.Photos)
+                .SingleOrDefaultAsync(u => u.UserNane == username);
+    public async Task<bool> SaveAllAsync()
+        => await context.SaveChangesAsync() > 0;
+    public void Update(AppUser user)
+        => context.Entry(user).State = EntityState.Modified;
 }
