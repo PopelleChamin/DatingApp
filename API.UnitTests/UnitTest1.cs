26c13bbb47463f1e3463df0802a9ab241266968d
Erick René Herrera González <mynama97@gmail.com>
2024-11-11 14:29:58 -0600

    Parcial03 - Add UNIT TESTS

---
+++ API.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace API.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+
+        //Act
+
+        //Assert
+        Assert.Equal(1, 0);
+    }
+}
