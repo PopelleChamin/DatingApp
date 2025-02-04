d1e30cf53ad583fe0bc4b6d608acd075eb048871
Erick René Herrera González <162051457+PopelleChamin@users.noreply.github.com>
2025-02-04 12:20:18 -0600

    Parcial03 (#4)
    * Parcial03 - Add members support
    
    * Parcial03 - Add member card component
    
    * Parcial03 - Add style to member cards
    
    * Parcial03 - Add style to cards
    
    * Parcial03 - Add JWT Interceptor
    
    * Parcial03 - Add routing to member detail page
    
    * Parcial03 - Add style to member detail component
    
    * Parcial03 - Add photo gallery
    
    * Parcial03 - Execute npm audit fix
    
    * Psrcial03 - Add member edit component
    
    * Parcial03 - Update member edit component
    
    Parcial03 - Update member edit component
    
    * Parcial03 - Add update profile
    
    * Parcial03 - Add CanDeactivate route guard
    
    * Parcial03 - Update user repository
    
    * Parcial03 - Update member update in SPA
    
    * Parcial03 - Add loading indicator
    
    * Parcial03 - Add data recovery from app state (cache)
    
    * Parcial03 - Add UNIT TESTS
    
    * Parcial03 - Add Seed Tests
    
    * Parcial03 - Update tests
    
    * Parcial0 - Add Minimun Unit Tests
    
    * Parcial03 - Add Excluded Coverages

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { Member } from '../../_models/member';
+import { MemberCardComponent } from '../member-card/member-card.component';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [],
+  imports: [MemberCardComponent],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit{
+  memberServices = inject(MembersService);
 
+  ngOnInit(): void {
+    if (this.memberServices.members().length == 0){
+      this.loadMembers();
+    }
+  }
+
+  loadMembers(){
+    this.memberServices.getMembers();
+  }
 }
