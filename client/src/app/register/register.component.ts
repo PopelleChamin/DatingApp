4d970ffec4bce3584443df2655c93ee363c4726e
Erick René Herrera González <mynama97@gmail.com>
2025-02-13 17:18:41 -0600

    Parcial04 - Add custom validation

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, input, OnInit, output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 
 @Component({
@@ -25,8 +25,18 @@ export class RegisterComponent implements OnInit{
     this.registerForm = new FormGroup({
       username: new FormControl("Hola", Validators.required),
       password: new FormControl("", [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl("", [Validators.required])
-    })
+      confirmPassword: new FormControl("", [Validators.required, this.matchValues("password")])
+    });
+
+    this.registerForm.controls["password"].valueChanges.subscribe({
+      next: () => this.registerForm.controls["confirmPassword"].updateValueAndValidity()
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {isMatching: true}
+    }
   }
 
   register(): void{
