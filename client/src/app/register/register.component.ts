7a1682d88b2991d58933d50455112298d3ef16be
Erick René Herrera González <mynama97@gmail.com>
2025-02-13 17:08:48 -0600

    Parcial04 - Add client validation

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject, input, OnInit, output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -18,13 +19,13 @@ export class RegisterComponent implements OnInit{
   registerForm: FormGroup = new FormGroup({});
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.initializeForm();
   }
   initializeForm() {
     this.registerForm = new FormGroup({
-      username: new FormControl(),
-      password: new FormControl(),
-      confirmPassword: new FormControl()
+      username: new FormControl("Hola", Validators.required),
+      password: new FormControl("", [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl("", [Validators.required])
     })
   }
 
