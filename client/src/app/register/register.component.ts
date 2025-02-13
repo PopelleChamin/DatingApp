73d317ebabd5f71b45403d6d68edbca07fb2423d
Erick René Herrera González <mynama97@gmail.com>
2025-02-13 16:33:36 -0600

    Parcial04 - Initialize Reactive form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,32 +1,44 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, input, OnInit, output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
   private accountService = inject(AccountService);
   private toastr = inject(ToastrService);
-  // usersFromHomeComponent= input.required<any>();
   cancelRegister = output<boolean>();
   model: any ={};
+  registerForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    })
+  }
 
   register(): void{
-    this.accountService.register(this.model).subscribe({
-      next: (response) =>{
-        console.log(response),
-        this.cancel();
-      },
-      error: (error) => {
-        this.toastr.error(error.errors);
-      }
-    });
+    console.log(this.registerForm.value);
+    //this.accountService.register(this.model).subscribe({
+    //  next: (response) =>{
+    //    console.log(response),
+    //    this.cancel();
+    //  },
+    //  error: (error) => {
+    //    this.toastr.error(error.errors);
+    //  }
+    //});
   }
 
   cancel(): void{
