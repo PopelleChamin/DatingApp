a7abd57b35a5549c742b654b6725ac12e0717ba2
Erick René Herrera González <162051457+PopelleChamin@users.noreply.github.com>
2025-02-25 16:35:03 -0600

    Parcial04 (#5)
    * Parcial04 - Add PhotoService
    
    * Parcial04 - Add photo uploader
    
    * Parcial04 - Add PUT for photos
    
    * Parcial04 - Add photo in client
    
    * Parcial04 - Set main photo in client
    
    * Parcial04 - Delete photo from API
    
    * Parcial04 - Delete photo from client
    
    * Parcial04 - Uploader Photos Solved
    
    * Parcial04 - Initialize Reactive form
    
    * Parcial04 - Add client validation
    
    * Parcial04 - Add custom validation
    
    * Parcial04 - Add feedback to FormGroup
    
    * Parcial04 - Add a reusable text input component
    
    * Parcial04 - Add more fields to registration form
    
    * Parcial04 - Add reusable date picker component
    
    * Parcial04 - Update API user registration
    
    * Parcial04 - Update client side user registration
    
    Parcial04 - Update client side user registration

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,14 +2,17 @@ namespace API.Controllers;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DataEntities;
 using API.DTOs;
 using API.Services;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 public class AccountController(
     DataContext context,
-    ITokenService tokenService) : BaseApiController
+    ITokenService tokenService,
+    IMapper mapper) : BaseApiController
 {
     [HttpPost("register")]
     public async Task<ActionResult<UserResponse>> RegisterAsync(RegisteRequest request)
@@ -20,29 +23,28 @@ public class AccountController(
             return BadRequest("Username already in use");
         }
 
-        return Ok();
+        using var hmac = new HMACSHA512();
+        var user = mapper.Map<AppUser>(request);
+        user.UserNane = request.Username;
+        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
+        user.PasswordSalt = hmac.Key;
 
-        //using var hmac = new HMACSHA512();
-        //var user = new AppUser
-        //{
-        //    UserNane = request.Username,
-        //    PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
-        //    PasswordSalt = hmac.Key
-        //};
-        //context.Users.Add(user);
-        //await context.SaveChangesAsync();
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
 
-        //return new UserResponse
-        //{
-        //    Username = user.UserNane,
-        //    Token = tokenService.CreateToken(user)
-        //};
+        return new UserResponse
+        {
+            Username = user.UserNane,
+            Token = tokenService.CreateToken(user),
+            KnownAs = user.KnownAs
+        };
     }
     [HttpPost("login")]
     public async Task<ActionResult<UserResponse>> LoginAsync(LoginRequest request)
     {
-        var user = await context.Users.FirstOrDefaultAsync(x =>
-         x.UserNane.ToLower() == request.Username.ToLower());
+        var user = await context.Users
+        .Include(x => x.Photos)
+        .FirstOrDefaultAsync(x => x.UserNane.ToLower() == request.Username.ToLower());
 
         if (user == null)
         {
@@ -62,7 +64,9 @@ public class AccountController(
         return new UserResponse
         {
             Username = user.UserNane,
-            Token = tokenService.CreateToken(user)
+            KnownAs = user.KnownAs,
+            Token = tokenService.CreateToken(user),
+            PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain)?.Url
         };
     }
 
