39ca6a6001e3d84b6276bf7ca1377ceaf255b858
Erick René Herrera González <mynama97@gmail.com>
2025-03-19 15:39:56 -0600

    Parcial05 - Add Message EF Migration

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -26,6 +26,16 @@ public class DataContext(DbContextOptions options) : DbContext(options)
             .HasForeignKey(f => f.TargetUserId)
             .OnDelete(DeleteBehavior.Cascade);
 
+        builder.Entity<Message>()
+            .HasOne(s => s.Recipient)
+            .WithMany(m => m.MessagesRecieved)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<Message>()
+            .HasOne(s => s.Sender)
+            .WithMany(m => m.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
+
     }
 }
 
