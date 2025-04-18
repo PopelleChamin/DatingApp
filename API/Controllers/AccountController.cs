0f574893eff1d2861aea4e1cdd2d73d3ab4ad053
Erick René Herrera González <mynama97@gmail.com>
2025-04-17 20:28:00 -0600

    Parcial06 - Add Identity entities

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -25,9 +25,9 @@ public class AccountController(
 
         using var hmac = new HMACSHA512();
         var user = mapper.Map<AppUser>(request);
-        user.UserNane = request.Username;
-        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
-        user.PasswordSalt = hmac.Key;
+        user.UserNane = request.Username.ToLowerInvariant();
+         // user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
+         // user.PasswordSalt = hmac.Key;
 
         context.Users.Add(user);
         await context.SaveChangesAsync();
@@ -52,16 +52,16 @@ public class AccountController(
             return Unauthorized("Invalid username or password");
         }
 
-        using var hmac = new HMACSHA512(user.PasswordSalt);
-        var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
+        //using var hmac = new HMACSHA512(user.PasswordSalt);
+        //var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
 
-        for (var i = 0; i < computeHash.Length; i++)
-        {
-            if (computeHash[i] != user.PasswordHash[i])
-            {
-                return Unauthorized("Invalid username or password");
-            }
-        }
+        //for (var i = 0; i < computeHash.Length; i++)
+        //{
+        //    if (computeHash[i] != user.PasswordHash[i])
+        //    {
+        //        return Unauthorized("Invalid username or password");
+        //    }
+        //}
         return new UserResponse
         {
             Username = user.UserNane,
