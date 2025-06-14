bf98722e95a361ab96331f83c4339fb76610a3bf
Erick René Herrera González <mynama97@gmail.com>
2025-06-14 15:57:43 -0600

    Parcial06 - Add optimizations to messages component
    Parcial06 - Add optimizations to messages component
    
    Parcial06 - Add optimizations to messages component
    
    Parcial06 - Add optimizations to messages component
    
    Parcial06 - Add optimizations to messages component

--- API/Data/UnitOfWork.cs
+++ API/Data/UnitOfWork.cs
@@ -1,12 +1,21 @@
-namespace API.UnitOfWork;
+namespace API.Data;
 
-using API.Data;
+using System.Threading.Tasks;
+using API.UnitOfWork;
 
-public interface IUnitOfWork
+public class UnitOfWork(
+    DataContext context,
+    IUserRepository userRepository,
+    ILikesRepository likesRepository,
+    IMessageRepository messageRepository) : IUnitOfWork
 {
-    public IMessageRepository MessageRepository { get; }
-    public ILikesRepository LikesRepository { get; }
-    public IUserRepository UserRepository { get; }
-    public Task<bool> Complete();
-    public bool HasChanges();
+    public IMessageRepository MessageRepository => messageRepository;
+
+    public ILikesRepository LikesRepository => likesRepository;
+
+    public IUserRepository UserRepository => userRepository;
+
+    public async Task<bool> Complete() => await context.SaveChangesAsync() > 0;
+
+    public bool HasChanges() => context.ChangeTracker.HasChanges();
 }
