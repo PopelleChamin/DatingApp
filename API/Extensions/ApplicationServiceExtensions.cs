dc3f50e47e166797a7672a8cdb42226f9cfb34da
Erick René Herrera González <mynama97@gmail.com>
2025-06-14 15:30:17 -0600

    Parcial06 - Add Unit of Work pattern

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -3,6 +3,7 @@ using API.Data;
 using API.Services;
 using API.Helpers;
 using API.SignalR;
+using API.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
 
 public static class ApplicationServiceExtensions
@@ -16,6 +17,7 @@ public static class ApplicationServiceExtensions
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<ILikesRepository, LikesRepository>();
         services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<IPhotoService, PhotoService>();
         services.AddScoped<LogUserActivity>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
