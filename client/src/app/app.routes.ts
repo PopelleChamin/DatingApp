6f31a2ef606fa5565ea070d5c77ce50fc3c81598
Erick René Herrera González <mynama97@gmail.com>
2025-06-14 11:33:00 -0600

    Parcial06 - Add Admin component and guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -11,6 +11,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { memberDetailedResolver } from './_resolvers/member-detailed.resolver';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { adminGuard } from './_guards/admin.guard';
 
 export const routes: Routes = [
     {path:"",component: HomeComponent},
@@ -33,4 +35,5 @@ export const routes: Routes = [
     {path: "not-found", component: NotFoundComponent},
     {path: "server-error", component: ServerErrorComponent},
     {path:"*",component: MessagesComponent, pathMatch: "full"},
+    {path: "admin", component: AdminPanelComponent, canActivate: [adminGuard]},
 ];
