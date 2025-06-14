a255461b650a69848aa405ebf594f4e5ec19dc7b
Erick René Herrera González <mynama97@gmail.com>
2025-06-14 13:25:56 -0600

    Parcial06 - Display online presence 2

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject, OnInit, ViewChild } from '@angular/core';
-import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabDirective, TabsetComponent, TabsModule } from 'ngx-bootstrap/tabs';
@@ -9,6 +8,7 @@ import { DatePipe } from '@angular/common';
 import { MemberMessagesComponent } from "../member-messages/member-messages.component";
 import { MessagesService } from '../../_services/messages.service';
 import { Message } from '../../_models/message';
+import { PresenceService } from '../../_services/presence.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -20,16 +20,15 @@ import { Message } from '../../_models/message';
 export class MemberDetailComponent implements OnInit {
   @ViewChild("memberTabs", { static: true }) memberTabs?: TabsetComponent;
   
-  private memberService = inject(MembersService);
   private messagesService = inject(MessagesService);
   private route = inject(ActivatedRoute);
+  presenceService = inject(PresenceService);
   member: Member = {} as Member;
   images: GalleryItem[] = [];
   activeTab?: TabDirective;
   messages: Message[] = [];
 
   ngOnInit(): void {
-    //this.loadMember();
 
     this.route.data.subscribe({
       next: data => {
